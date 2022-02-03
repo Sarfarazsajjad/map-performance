cb1d4403fa9aabae96d5ad11558e5dcdd15d0957
Hamza Azam <hamzaazam430@outlook.com>
2022-02-03 12:56:03 +0500

    modifies map script

--- script.js
+++ script.js
@@ -40,22 +40,12 @@ else if (dataDisplayType == 'cluster') {
         spiderfyOnMaxZoom: true,
         showCoverageOnHover: false,
         zoomToBoundsOnClick: false,
-        // disableClusteringAtZoom: 18,
         maxClusterRadius: 80,
     });
 
     for (let y = 0; y < totalDataPoints; y++) {    
         markers.addLayer(L.marker([Math.floor(Math.random() * totalDataPoints * 0.1), Math.floor(Math.random() * totalDataPoints * 0.1)]).bindPopup("Iteration No: "+iteration));
-        // for (var x = 0; x < totalDataPoints; x++) {
-        //     markers.addLayer(L.marker([48.5+(x*0.1), 20.1+(y*0.1)]).bindPopup("Iteration No: "+iteration));
-            // markers.addLayer(L.circleMarker([48.5+(x*0.1), 20.1+(y*0.1)], {
-            //     color: 'red',
-            //     fillColor: '#234',
-            //     fillOpacity: 1,
-            //     radius: 10
-            // }).bindPopup("Iteration No: "+iteration));
-            iteration++;
-        // }
+        iteration++;
     }
     map.addLayer(markers);
     map.fitBounds(markers.getBounds());
@@ -66,31 +56,8 @@ else if (dataDisplayType == 'heat') {
     var points = [];
 
     for (let y = 0; y < totalDataPoints; y++) {    
-        // for (var x = 0; x < totalDataPoints; x++) {
-            // points.push({lat: (48.5+(x*0.1)), lng: (20.1+(y*0.1)), count: 1});
-            points.push({lat: (Math.floor(Math.random() * totalDataPoints * 0.1)), lng: (Math.floor(Math.random() * totalDataPoints * 0.1)), count: 1});
-            iteration++;
-        // }
-    }
-
-    var testData = {
-        max: 8,
-        data: points,//[{lat: 24.6408, lng:46.7728, count: 1},{lat: 24.7408, lng:46.8728, count: 1},{lat: 50.75, lng:-1.55, count: 1}]
-    };
-    map.addLayer(heatmapLayer);
-    heatmapLayer.setData(testData);
-
-} 
-else if (dataDisplayType == 'll_heat') {
-
-    var points = [];
-
-    for (let y = 0; y < totalDataPoints; y++) {    
-        // for (var x = 0; x < totalDataPoints; x++) {
-            // points.push({lat: (48.5+(x*0.1)), lng: (20.1+(y*0.1)), count: 1});
-            points.push([(Math.floor(Math.random() * totalDataPoints * 0.1)), (Math.floor(Math.random() * totalDataPoints * 0.1)), "500"]);
-            iteration++;
-        // }
+        points.push([(Math.floor(Math.random() * totalDataPoints * 0.1)), (Math.floor(Math.random() * totalDataPoints * 0.1)), "500"]);
+        iteration++;
     }
 
     var heatLayer = L.heatLayer(points,{radius: 25}).addTo(map);
@@ -99,20 +66,5 @@ else if (dataDisplayType == 'll_heat') {
 
 } 
 
-
-
-
 console.log("Value Given: ",totalDataPoints);
 console.log("Total Data Points Generated: ",iteration);
-
-
-
-// for (let y = 0; y < totalDataPoints; y++) {    
-    // markers.addLayer(L.circleMarker([(Math.floor(Math.random() * totalDataPoints * 0.5)), (Math.floor(Math.random() * totalDataPoints * 0.5))], {
-//         color: 'red',
-//         fillColor: '#234',
-//         fillOpacity: 1,
-//         radius: 10
-//     })/*.addTo(map)*/.bindPopup("Iteration No: "+iteration));
-//     iteration++;
-// };
